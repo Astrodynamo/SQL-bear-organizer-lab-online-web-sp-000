e379f0a3044da51681a07bfadc7110efb7a50dee
Amanda King <amapickell@gmail.com>
2020-05-04 14:10:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 25, "female", "brown", "active", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "male", "brown", "cautious", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 11, "female", "dark brown", "cautious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NILL, 28, "male", "brown", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, "male", "brown", "aggressive", 0);
